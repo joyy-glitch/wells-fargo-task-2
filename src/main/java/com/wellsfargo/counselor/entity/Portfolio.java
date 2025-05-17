1e557ce3c6a0fd98aa07b83db0a18e8927bd6ff8
Joy Addy <joyaddy267@gmail.com>
2025-05-17 22:12:19 +0000

    Added Portfolio class

---
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -0,0 +1,29 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Portfolio {
+    @Id
+    @GeneratedValue
+    private long portfolioId;
+
+    @Column(nullable = false)
+    private String portfolioDescription;
+
+    @OneToOne
+    @JoinColumn(name = "clientId", referencedColumnName = "clientId")
+    private Client client;
+    protected Portfolio() {
+
+    }
+    public  Portfolio(String portfolioDescription) {
+        this.portfolioDescription = portfolioDescription;
+
+    }
+    public long getPortfolioId() {return portfolioId; }
+
+    public void setPortfolioId(long portfolioId) {
+        this.portfolioId = portfolioId;
+    }
+}
