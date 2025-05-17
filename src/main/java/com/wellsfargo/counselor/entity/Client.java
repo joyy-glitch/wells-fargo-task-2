a81144dcffc7e3344afecc81e649a03e9ef156e4
Joy Addy <joyaddy267@gmail.com>
2025-05-17 22:04:33 +0000

    Added Client class

---
+++ src/main/java/com/wellsfargo/counselor/entity/Client.java
@@ -0,0 +1,65 @@
+
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+
+
+@Entity
+public class Client {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private long clientId;
+
+    @Column(nullable = false)
+    private String clientName;
+
+    @Column(nullable = false)
+    private String clientEmail;
+
+    @Column(nullable = false)
+    private String clientAddress;
+
+    @ManyToOne
+    @JoinColumn(name = "advisorId") // Foreign key column in the 'Client' table
+    private Advisor advisor;
+
+
+    protected Client() {}
+
+    public Client( String clientName, String clientAddress, String clientEmail ) {
+        this.clientName = clientName;
+        this.clientAddress = clientAddress;
+        this.clientEmail = clientEmail;
+
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public String getClientEmail() {
+        return clientEmail;
+    }
+
+    public void setClientEmail(String clientEmail) {
+        this.clientEmail = clientEmail;
+    }
+
+    public String getClientAddress() {
+        return clientAddress;
+    }
+
+    public void setClientAddress(String clientAddress) {
+        this.clientAddress = clientAddress;
+    }
+}
